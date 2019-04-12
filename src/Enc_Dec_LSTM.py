aaab49f7f74d1bc7e84e476b1f4813344ae4d1ad
Ubuntu <ubuntu@ip-172-31-31-171.eu-west-1.compute.internal>
2019-04-12 17:15:44 +0000

    moving prediction file to local

--- src/Enc_Dec_LSTM.py
+++ src/Enc_Dec_LSTM.py
@@ -36,98 +36,107 @@ test_y = test_y.reshape(test_rows, 1)
 epochs = 10000
 batch_size, features, timesteps = rows, cols, rows
 
-#if a model checkpoint does not exist
-#then load it and continue from there
-try:
-    _ = open('/dltraining/checkpoints/best_checkpoint.hdf5', 'r') # file exists
-
-
-    print("model from checkpoint")
-    # define model
-    model = Sequential()
-
-    model.add(LSTM(400, activation='relu', input_shape=(1, features)))
-
-    model.add(RepeatVector(1))
-
-    model.add(LSTM(400, activation='relu', return_sequences=True))
-    model.add(LSTM(400, activation='relu', return_sequences=True))
-
-    model.add(TimeDistributed(Dense(400, activation='relu')))
-    model.add(TimeDistributed(Dense(1)))
-
-    model.compile(loss='mse', optimizer='adam')
+def build_model():
+    #if a model checkpoint does not exist
+    #then load it and continue from there
+    try:
+        _ = open('/dltraining/checkpoints/best_checkpoint.hdf5', 'r') # file exists
     
-    #create early stopping callback
-    early_stop = EarlyStopping(monitor='loss',
-                               mode='min',
-                               verbose=1,
-                               patience=200)
     
-    #create a checkpoint callback
-    filepath="/dltraining/checkpoints/"
-    checkpt = ModelCheckpoint(filepath+"best_checkpoint.hdf5", 
-                              monitor='loss',
-                              verbose=1,
-                              save_best_only=True,
-                              mode='min')
-
-    #call back to log the loss
-    csv_logger = CSVLogger(filepath+'log.csv', 
-                           append=True, 
-                           separator=',')
-
-    callbacks_list = [checkpt, early_stop, csv_logger]
-
-    #load weights
-    model.load_weights("/dltraining/checkpoints/best_checkpoint.hdf5")
-
-    # fit network
-    model.fit(train_x, train_y,
-              epochs=epochs,
-              batch_size=batch_size,
-              verbose=0,
-              callbacks=callbacks_list)
-except FileNotFoundError:
-    print("From Main")
-    model = Sequential()
-
-    model.add(LSTM(400, activation='relu', input_shape=(1, features)))
-
-    model.add(RepeatVector(1))
-
-    model.add(LSTM(400, activation='relu', return_sequences=True))
-    model.add(LSTM(400, activation='relu', return_sequences=True))
-
-    model.add(TimeDistributed( Dense(400, activation='relu')))
-    model.add(TimeDistributed( Dense(1, activation='relu')))
-
-    model.compile( loss='mse', optimizer='adam')
+        print("model from checkpoint")
+        # define model
+        model = Sequential()
     
-    #create an early stopping callback
-    early_stop = EarlyStopping(monitor='loss',
-                               mode='min',
-                               verbose=1,
-                               patience=200)
+        print(features)
+        model.add(LSTM(400, activation='relu', input_shape=(1, features)))
     
-    #create a checkpoint callback
-    filepath="/dltraining/checkpoints/"
-    checkpt = ModelCheckpoint(filepath+"best_checkpoint.hdf5", 
-                              monitor='loss',
-                              verbose=1,
-                              save_best_only=True, 
-                              mode='min')
+        model.add(RepeatVector(1))
+    
+        model.add(LSTM(400, activation='relu', return_sequences=True))
+        model.add(LSTM(400, activation='relu', return_sequences=True))
+    
+        model.add(TimeDistributed(Dense(400, activation='relu')))
+        model.add(TimeDistributed(Dense(1)))
+    
+        model.compile(loss='mse', optimizer='adam')
+        
+        #create early stopping callback
+        early_stop = EarlyStopping(monitor='loss',
+                                   mode='min',
+                                   verbose=1,
+                                   patience=200)
+        
+        #create a checkpoint callback
+        filepath="/dltraining/checkpoints/"
+        checkpt = ModelCheckpoint(filepath+"best_checkpoint.hdf5", 
+                                  monitor='loss',
+                                  verbose=1,
+                                  save_best_only=True,
+                                  mode='min')
+    
+        #call back to log the loss
+        csv_logger = CSVLogger(filepath+'log.csv', 
+                               append=True, 
+                               separator=',')
+    
+        callbacks_list = [checkpt, early_stop, csv_logger]
+    
+        #load weights
+        model.load_weights("/dltraining/checkpoints/best_checkpoint.hdf5")
+    
+        # fit network
+        model.fit(train_x, train_y,
+                  epochs=epochs,
+                  batch_size=batch_size,
+                  verbose=0,
+                  callbacks=callbacks_list)
+        return model
+    except FileNotFoundError:
+        print("From Main")
+        model = Sequential()
+    
+        model.add(LSTM(400, activation='relu', input_shape=(1, features)))
+    
+        model.add(RepeatVector(1))
+    
+        model.add(LSTM(400, activation='relu', return_sequences=True))
+        model.add(LSTM(400, activation='relu', return_sequences=True))
+    
+        model.add(TimeDistributed( Dense(400, activation='relu')))
+        model.add(TimeDistributed( Dense(1, activation='relu')))
+    
+        model.compile( loss='mse', optimizer='adam')
+        
+        #create an early stopping callback
+        early_stop = EarlyStopping(monitor='loss',
+                                   mode='min',
+                                   verbose=1,
+                                   patience=200)
+        
+        #create a checkpoint callback
+        filepath="/dltraining/checkpoints/"
+        checkpt = ModelCheckpoint(filepath+"best_checkpoint.hdf5", 
+                                  monitor='loss',
+                                  verbose=1,
+                                  save_best_only=True, 
+                                  mode='min')
+    
+        #call back to log the loss
+        csv_logger = CSVLogger(filepath+'log.csv', 
+                               append=True, 
+                               separator=',')
+        callbacks_list = [checkpt, early_stop, csv_logger]
+    
+        model.fit(train_x, train_y,
+                  epochs=epochs, 
+                  batch_size=batch_size, 
+                  verbose=0, 
+                  callbacks=callbacks_list)
+        return model
 
-    #call back to log the loss
-    csv_logger = CSVLogger(filepath+'log.csv', 
-                           append=True, 
-                           separator=',')
-    callbacks_list = [checkpt, early_stop, csv_logger]
+lstm_model = build_model()
+pred_y = lstm_model.predict(test_x)
 
-    model.fit(train_x, train_y,
-              epochs=epochs, 
-              batch_size=batch_size, 
-              verbose=0, 
-              callbacks=callbacks_list)
 
+np.savetxt("output.txt", X=pred_y.reshape(pred_y.shape[0], 1), delimiter=',')
 
